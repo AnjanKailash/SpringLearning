40c8503e15de046fccb47abfc3ddfea3cf6ff30e
AnjanKailash <anjan.kailash@gmail.com>
2021-05-16 11:04:27 +0530

    context started listener

--- AlmightyJava/05_SpringCore-Listeners/src/com/spring/core/listeners/ContextStartListener.java
+++ AlmightyJava/05_SpringCore-Listeners/src/com/spring/core/listeners/ContextStartListener.java
@@ -4,11 +4,12 @@ import java.util.Date;
 
 import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextStartedEvent;
 
-public class ContextStartListener implements ApplicationListener<ApplicationEvent> {
+public class ContextStartListener implements ApplicationListener<ContextStartedEvent> {
 
 	@Override
-	public void onApplicationEvent(ApplicationEvent event) {
+	public void onApplicationEvent(ContextStartedEvent event) {
 		// TODO Auto-generated method stub
 		System.out.println("Context start event received: "+event.getSource()+" "+new Date());
 	}
